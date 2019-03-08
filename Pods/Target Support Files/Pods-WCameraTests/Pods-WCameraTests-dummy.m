8d5aecdcc8c58c4f4224bdd59c4b23ad38f50b02
halseywu <wsh940808@163.com>
2019-03-08 16:16:21 +0800

    创建了项目

---
+++ Pods/Target Support Files/Pods-WCameraTests/Pods-WCameraTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WCameraTests : NSObject
+@end
+@implementation PodsDummy_Pods_WCameraTests
+@end
---
+++ WCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WCamera
+//
+//  Created by HalseyW-15 on 2019/3/8.
+//  Copyright © 2019 wushhhhhh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
