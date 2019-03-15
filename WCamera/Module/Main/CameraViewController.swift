87257cab80164bf1f66022d213f0592c86b5c4dd
halseywu <wsh940808@163.com>
2019-03-15 10:11:42 +0800

    增加了拍照并保存至相册的功能

--- WCamera/Module/Main/CameraViewController.swift
+++ WCamera/Module/Main/CameraViewController.swift
@@ -8,35 +8,69 @@
 
 import UIKit
 import AVFoundation
+import Photos
 
 class CameraViewController: UIViewController {
     var previewView: PreviewView?
+    var btnCapturePhoto: UIButton?
+    var photoOutput: AVCapturePhotoOutput?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         initView()
         
         let captureSession = AVCaptureSession()
-        
         captureSession.beginConfiguration()
         let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .unspecified)
         guard let deviceInput = try? AVCaptureDeviceInput(device: device!), captureSession.canAddInput(deviceInput) else { return }
         captureSession.addInput(deviceInput)
-        
-        let photoOutput = AVCapturePhotoOutput()
-        guard captureSession.canAddOutput(photoOutput) else {
+         photoOutput = AVCapturePhotoOutput()
+        guard let photoOutput = photoOutput, captureSession.canAddOutput(photoOutput) else {
             return
         }
-        
         captureSession.sessionPreset = .photo
         captureSession.addOutput(photoOutput)
         captureSession.commitConfiguration()
-        
         previewView?.videoPreviewLayer.session = captureSession
-        
         captureSession.startRunning()
     }
+    
+    @objc func onClickCapturePhotoButton() {
+        capturePhoto()
+    }
+    
+    func capturePhoto() {
+        guard let photoOutput = photoOutput else { return }
+        let photoSettings: AVCapturePhotoSettings
+        if photoOutput.availablePhotoCodecTypes.contains(.hevc) {
+            photoSettings = AVCapturePhotoSettings.init(format: [AVVideoCodecKey: AVVideoCodecType.hevc])
+        } else {
+            photoSettings = AVCapturePhotoSettings.init()
+        }
+        photoSettings.flashMode = .auto
+        photoSettings.isAutoStillImageStabilizationEnabled = photoOutput.isStillImageStabilizationSupported
+        photoOutput.capturePhoto(with: photoSettings, delegate: self)
+    }
+}
 
+extension CameraViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let e = error {
+            print(e.localizedDescription)
+            return
+        }
+        PHPhotoLibrary.shared().performChanges({
+            let creationRequest = PHAssetCreationRequest.forAsset()
+            creationRequest.addResource(with: .photo, data: photo.fileDataRepresentation()!, options: nil)
+        }) { (isSuccess, error) in
+            if isSuccess {
+                print("Capture Success")
+            }
+            if let e = error {
+                print("Capture Error: \(e.localizedDescription)")
+            }
+        }
+    }
 }
 
 extension CameraViewController {
@@ -49,5 +83,18 @@ extension CameraViewController {
             make.width.equalToSuperview()
             make.height.equalToSuperview()
         })
+        //拍照按钮
+        btnCapturePhoto = UIButton.init()
+        btnCapturePhoto?.setTitle("拍照", for: .normal)
+        btnCapturePhoto?.setTitleColor(.white, for: .normal)
+        btnCapturePhoto?.backgroundColor = .blue
+        self.view.addSubview(btnCapturePhoto!)
+        btnCapturePhoto?.snp.makeConstraints({ (make) in
+            make.width.equalTo(100)
+            make.height.equalTo(50)
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+        })
+        btnCapturePhoto?.addTarget(self, action: #selector(onClickCapturePhotoButton), for: .touchUpInside)
     }
 }
