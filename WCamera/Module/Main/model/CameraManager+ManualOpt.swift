088a5ee2aaaa5b171561fcbb5921032f76b4dbaf
halseywu <wsh940808@163.com>
2019-04-15 17:04:19 +0800

    修改EV调整为整数值

--- WCamera/Module/Main/model/CameraManager+ManualOpt.swift
+++ WCamera/Module/Main/model/CameraManager+ManualOpt.swift
@@ -72,8 +72,9 @@ extension CameraManager {
     /// - Parameter ev: 曝光补偿度
     func changeEV(to ev: Float) {
         guard let device = captureDevice else { return }
+        let value = Float(lroundf(ev))
         device.changeProperty {
-            $0.setExposureTargetBias(ev) { _ in
+            $0.setExposureTargetBias(value) { _ in
                 self.delegate?.didChangeEvValue(to: ev)
             }
         }
